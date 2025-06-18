65a348f32a889abdd2806863fbfc47eb7c685ecf
Isra Ragheb <isra.ismail@student.guc.edu.eg>
2025-06-18 16:26:53 +0300

    rearrange script

---
+++ ascii-script.sh
@@ -0,0 +1,8 @@
+sudo apt install cowsay -y
+Generate ASCII Art
+cowsay -f dragon "I am a sassy dragon :p" >> dragon.txt
+grep -i "dragon" dragon.txt 
+Read file
+cat dragon.txt
+Check repo files
+ls -ltra
