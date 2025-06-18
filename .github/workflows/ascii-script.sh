2b5e3d2729e69e3527598440d142df0aa5d5596c
Isra Ragheb <isra.ismail@student.guc.edu.eg>
2025-06-18 16:27:09 +0300

    rearrange script

--- .github/workflows/ascii-script.sh
+++
@@ -1,8 +0,0 @@
-sudo apt install cowsay -y
-Generate ASCII Art
-cowsay -f dragon "I am a sassy dragon :p" >> dragon.txt
-grep -i "dragon" dragon.txt 
-Read file
-cat dragon.txt
-Check repo files
-ls -ltra
